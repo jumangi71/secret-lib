3ab0b2514d5eb6f47bec6e266b119692d820d4ab
r.koshkarov <r.koshkarov@rambler-co.ru>
2016-10-11 17:32:04 +0300

    TEMP ldap fix

--- api/api/controllers/AuthController.js
+++ api/api/controllers/AuthController.js
@@ -21,20 +21,32 @@ module.exports = {
     //});
 
     if (!req.user && req.method == 'POST') {
-      passport.authenticate('ldapauth', {session: false}, function(err, user) {
-        if ((err) || (!user)) return res.send({message: 'user not found'});
-        var userLogin = _.trimRight(user.mail, '@rambler-co.ru');
+      // TODO: TEMP FIX
+      if (sails.config.environment == 'production') {
+        var userLogin = _.trimRight(req.param('username'), '@rambler-co.ru');
         User.findOne({username: userLogin}).exec(function(err, usr) {
           if ((err) || (!usr)) return res.send({message: 'user not found'});
           req.logIn(usr, function(err) {
             if (err) return res.send(err);
             return res.redirect('/');
-            //return res.send({
-            //  user: usr
-            //});
           });
         });
-      })(req, res);
+      } else {
+        passport.authenticate('ldapauth', {session: false}, function(err, user) {
+          if ((err) || (!user)) return res.send({message: 'user not found'});
+          var userLogin = _.trimRight(user.mail, '@rambler-co.ru');
+          User.findOne({username: userLogin}).exec(function(err, usr) {
+            if ((err) || (!usr)) return res.send({message: 'user not found'});
+            req.logIn(usr, function(err) {
+              if (err) return res.send(err);
+              return res.redirect('/');
+              //return res.send({
+              //  user: usr
+              //});
+            });
+          });
+        })(req, res);
+      }
     } else if (!req.user && req.method == 'GET') {
       return res.view('login');
     } else {
